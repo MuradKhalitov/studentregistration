51e0d697059b07df3643d731e3d6517889ffb354
Murad Khalitov <darggun@gmail.com>
2024-04-15 15:10:20 +0300

    init

---
+++ src/test/java/ru/skillbox/student/registration/StudentRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.student.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
